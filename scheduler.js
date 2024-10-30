9c3b787d226b311346d6d50109894d751c91d646
Raghul Prasanth <87120429+Raghul-18@users.noreply.github.com>
2024-10-30 21:22:23 +0530

    Delete scheduler.js

--- scheduler.js
+++
@@ -1,9 +0,0 @@
-const cron = require('node-cron');
-
-// Schedule a task to run every minute
-cron.schedule('* * * * *', () => {
-  console.log('Task is running every minute');
-  // Add any additional scheduled tasks here
-});
-
-console.log("Scheduler is active. Task will run every minute.");
